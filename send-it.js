3858c2583887acd63045677cb9e4e2f82e68b213
CamdynR <rasque.camdyn@gmail.com>
2022-01-28 14:48:10 -0800

    Testing using custom storage with multer

--- send-it.js
+++ send-it.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
     callback(null, dir);
   },
   filename: (req, file, callback) => {
-    callback(null, file.filename + '');
+    callback(null, file.originalname);
   }
 });
 const upload = multer({ dest: 'uploads/', preservePath: true, storage: storage });
